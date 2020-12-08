50d9271a6e83db146308b4c08cad1697afc13cfc
snehadubey9066 <59720911+snehadubey9066@users.noreply.github.com>
2020-12-08 14:05:32 +0530

    Written code for the diode to work according to the input formula

---
+++ Ques10_code.m
@@ -0,0 +1,14 @@
+data = xlsread('diode_eqn_excel.xlsx');
+R = 330;
+Vd = 0.7;
+V = data;
+Id = (V-Vd)/R;
+sim("Diode_Lookup.slx")
+% 
+% Vt = 26*10^-3;
+% n=1;
+% Io = 10^-14; 
+% V = data;
+% Id = Io*(exp(V/n*Vt)-1);
+
+sim("Ques10_sys.slx")
